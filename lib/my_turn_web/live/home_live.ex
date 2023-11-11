d0b5654f7624d117a01ef695649f7f826b926e89
Thomas Coopman <thomas.coopman@gmail.com>
2023-11-11 10:06:21 +0100

    layout + handle leave

--- lib/my_turn_web/live/home_live.ex
+++ lib/my_turn_web/live/home_live.ex
@@ -6,19 +6,19 @@ defmodule MyTurnWeb.HomeLive do
 
   def render(assigns) do
     ~H"""
-    <div class="bg-gray-100 w-screen h-screen flex flex-col justify-between">
+    <div class="w-screen h-[100dvh] flex flex-col justify-between">
       <div class="h-full flex-grow flex justify-center items-center">
         Current turn: <%= @turn %>
       </div>
-      <div class="h-20 flex gap-4 justify-center flex-shrink-0 items-center">
+      <div class="h-40 flex gap-4 justify-center flex-shrink-0 items-center">
         <button
-          class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500"
+          class="rounded-full bg-sky-300 h-16 w-32 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500"
           phx-click="join"
         >
           Queue
         </button>
         <button
-          class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500"
+          class="rounded-full bg-sky-300  h-16 w-32 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500"
           phx-click="leave"
         >
           Leave
@@ -61,6 +61,12 @@ defmodule MyTurnWeb.HomeLive do
     key = socket.assigns.key
 
     {:noreply, assign(socket, :turn, turn(key))}
+  end
+
+  def terminate(_reson, socket) do
+    key = socket.assigns.key
+    :ok = MyTurn.Queue.leave(key)
+    :ok
   end
 
   defp turn(key) do
